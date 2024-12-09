4adaeed626530c1e07779c85bc40c89b89f3c772
Mhando-Z <Mhandosz17@gmail.com>
2024-12-09 18:14:01 +0300

    Phase Two

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "../Components/ProductCard";
 import { RxCrossCircled } from "react-icons/rx";
 import data from "../lib/datas";
-
 import { openDB } from "idb";
 
 const dbPromise = openDB("MyDatabase", 1, {
@@ -16,6 +15,9 @@ const dbPromise = openDB("MyDatabase", 1, {
 function Home() {
   const [selected, setSelect] = useState([]);
   const [cartData, setCart] = useState([]);
+
+  // total sum of all items in cart
+  const total = cartData.reduce((acc, item) => acc + item.price, 0);
 
   // adding data
   const addData = async () => {
@@ -54,6 +56,7 @@ function Home() {
     try {
       await db.delete("myStore", id);
       console.log(`Item with id ${id} deleted successfully.`);
+      getAllData();
     } catch (error) {
       console.error(`Failed to delete item with id ${id}:`, error);
     }
@@ -86,24 +89,43 @@ function Home() {
           })}
         </div>
         {/* cart section */}
-        <div className="bg-white w-[550px] h-[500px] rounded-2xl p-5 ">
-          <h1 className="text-2xl font-bold text-orange-600">
-            Your Cart ({cartData?.length})
-          </h1>
-          <div className="flex flex-col gap-3 mt-4">
-            {cartData?.map((dt) => {
-              return (
-                <div key={dt.id} className="flex items-center justify-between">
-                  <div>
-                    <h1 className="font-bold ">{dt.name}</h1>
-                    <p>$ {dt?.price}</p>
+        <div className="flex w-[550px] flex-col gap-y-5">
+          <div className="bg-white flex flex-col  h-[500px] overflow-y-auto rounded-2xl px-5 ">
+            <h1 className="py-5 text-2xl font-bold text-orange-600 bg-white ">
+              Your Cart ({cartData?.length})
+            </h1>
+
+            <div className="flex flex-col gap-3 mt-5 ">
+              {cartData?.map((dt) => {
+                return (
+                  <div
+                    key={dt.id}
+                    className="flex items-center justify-between"
+                  >
+                    <div>
+                      <h1 className="font-bold ">{dt.name}</h1>
+                      <p>$ {dt?.price}</p>
+                    </div>
+                    <div>
+                      <RxCrossCircled
+                        onClick={() => deleteData(dt?.id)}
+                        className="text-xl cursor-pointer hover:text-red-600"
+                      />
+                    </div>
                   </div>
-                  <div>
-                    <RxCrossCircled className="text-xl cursor-pointer hover:text-red-600" />
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
+          </div>
+          {/* total sum */}
+          <div className="bg-white px-5 h-[100px] border-t border-slate-500 mt-2">
+            <div className="flex flex-row items-center justify-between mt-3">
+              <h1 className="font-medium">Order Total</h1>
+              <h1 className="font-bold">$ {total}</h1>
+            </div>
+            <div className="items-center w-full py-2 mt-4 text-center text-white bg-red-600 rounded-3xl ">
+              <h1 className="font-bold">Confirm Order</h1>
+            </div>
           </div>
         </div>
       </div>
