71683504292487c8511f69cdda9f334630667e5a
Mhando-Z <Mhandosz17@gmail.com>
2024-12-11 19:12:50 +0300

    Phase One'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Footer from "./Components/Footer";
+import NavBar from "./Components/NavBar";
 import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden bg-orange-50">
+    <div className="flex flex-col justify-between min-h-screen overflow-hidden bg-white">
+      <NavBar />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+
+function NavBar() {
+  return (
+    <div className="fixed z-50 w-full mt-5">
+      <div className="flex flex-row items-center justify-between w-full px-5 py-3 lg:container lg:mx-auto lg:px-0">
+        {/* logo and nav links*/}
+        <div className="flex items-center gap-x-5">
+          <img src={logo} alt="shortly logo" />
+          <div className="flex flex-row items-center gap-x-4">
+            <button className="font-semibold hover:font-bold ">Features</button>
+            <button className="font-semibold hover:font-bold ">Pricing</button>
+            <button className="font-semibold hover:font-bold ">
+              Resources
+            </button>
+          </div>
+        </div>
+        {/* nav links */}
+        <div className="flex items-center gap-x-4">
+          <button className="font-semibold hover:font-bold">Login</button>
+          <button className="py-1.5 px-4 bg-cyan-600 text-white font-bold rounded-3xl">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
