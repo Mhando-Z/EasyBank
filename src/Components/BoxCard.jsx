00b6ca267323226041fd79be10a7045a18431794
Mhando-Z <Mhandosz17@gmail.com>
2025-03-06 16:14:45 +0300

    Phase Two

---
+++ src/Components/BoxCard.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const BoxCard = ({ icon, title, description }) => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const cardVariants = {
+    hidden: { y: 50, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 12,
+      },
+    },
+    hover: {
+      y: -10,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 10,
+      },
+    },
+  };
+
+  return (
+    <div className="flex items-center justify-center py-4 ">
+      <motion.div
+        className="flex flex-col w-full max-w-6xl gap-6 md:flex-row md:gap-4"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.div
+          className="relative flex-1 p-10 bg-white rounded-lg shadow-lg"
+          variants={cardVariants}
+          whileHover="hover"
+        >
+          <motion.div
+            className="absolute flex items-center justify-center w-16 h-16 text-2xl rounded-full bg-blue-950 text-cyan-400 -top-8 left-6"
+            whileHover={{ rotate: 360 }}
+            transition={{ duration: 0.6 }}
+          >
+            <img src={icon} alt="logo" className="object-cover" />
+          </motion.div>
+          <div className="mt-8">
+            <h3 className="mb-3 text-xl font-bold text-gray-800">{title}</h3>
+            <p className="text-gray-500">{description}</p>
+          </div>
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default BoxCard;
--- src/Components/HeroSect.jsx
+++ src/Components/HeroSect.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import BoxCard from "./BoxCard";
 // image import
 import mage from "../assets/images/illustration-working.svg";
 import mage2 from "../assets/images/bg-shorten-desktop.svg";
+import icon1 from "../assets/images/icon-detailed-records.svg";
+import icon2 from "../assets/images/icon-brand-recognition.svg";
+import icon3 from "../assets/images/icon-fully-customizable.svg";
 
 function HeroSect() {
   return (
@@ -48,20 +52,54 @@ function HeroSect() {
       </div>
       {/* third section */}
       <div className="-mt-12 bg-gray-200 ">
-        <div className="flex flex-col items-center gap-2 mt-28 ">
+        <div className="flex flex-col items-center gap-2 mt-32">
           <h1 className="text-4xl font-bold">Advanced Statistics </h1>
           <p className="max-w-md text-gray-400 md:text-center">
             frack how your tinks are pertamning dorast the web with our
             odvaricod stutistics shbooOTE
           </p>
         </div>
-        <div className="flex container mx-auto flex-col relative mt-16 h-[500px] ">
+        <div className="flex md:container md:mx-auto px-4 md:px-0  flex-col  relative  h-[500px] ">
           {/* horizontal line */}
           <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center">
-            <div className="w-full h-3 bg-cyan-500"></div>
+            <div className="hidden w-full h-3 bg-cyan-500 md:flex"></div>
+          </div>
+
+          {/* cards */}
+          <div className="absolute top-0 bottom-0 flex flex-col items-center justify-between w-full h-full gap-10 md:flex-row">
+            {/* first cards */}
+            <BoxCard
+              icon={icon2}
+              title={"Brand Recognition"}
+              description={
+                "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
+              }
+            />
+            {/* second card */}
+            <div className="md:mt-28">
+              <BoxCard
+                icon={icon1}
+                title={"Detailed Records"}
+                description={
+                  "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
+                }
+              />
+            </div>
+            <div className="md:mt-44">
+              {/* third caord */}
+              <BoxCard
+                icon={icon3}
+                title={"Fully Customizable"}
+                description={
+                  "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+                }
+              />
+            </div>
           </div>
         </div>
       </div>
+      {/* fourth section */}
+      <div></div>
     </div>
   );
 }
