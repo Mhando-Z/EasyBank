771f66996967ff650a4865d7f144fc19684c4300
Mhando-Z <Mhandosz17@gmail.com>
2025-03-07 10:47:52 +0300

    Phase Two

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -7,8 +7,8 @@ function NavBar() {
       <div className="flex flex-row items-center justify-between w-full px-5 py-3 lg:container lg:mx-auto lg:px-0">
         {/* logo and nav links*/}
         <div className="flex items-center gap-x-5">
-          <img src={logo} alt="shortly logo" />
-          <div className="flex flex-row items-center gap-x-4">
+          <img src={logo} alt="shortly logo" className="h-6" />
+          <div className="flex-row items-center hidden gap-x-4 md:flex">
             <button className="font-semibold hover:font-bold ">Features</button>
             <button className="font-semibold hover:font-bold ">Pricing</button>
             <button className="font-semibold hover:font-bold ">
@@ -17,7 +17,7 @@ function NavBar() {
           </div>
         </div>
         {/* nav links */}
-        <div className="flex items-center gap-x-4">
+        <div className="items-center hidden md:flex gap-x-4">
           <button className="font-semibold hover:font-bold">Login</button>
           <button className="py-1.5 px-4 bg-cyan-600 text-white font-bold rounded-3xl">
             Sign Up
