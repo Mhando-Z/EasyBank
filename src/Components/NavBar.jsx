74840baa48840de127dcd1d5c1b67556b6f8434e
Mhando-Z <Mhandosz17@gmail.com>
2025-03-09 15:27:57 +0300

    phase One

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -10,24 +10,25 @@ function NavBar() {
     setShow(!show);
   };
   return (
-    <div className="fixed z-50 w-full bg-white">
+    <div className="fixed z-50 w-full bg-white ">
       <div className="relative flex flex-row items-center justify-between w-full px-5 py-3 lg:container lg:mx-auto lg:px-0">
         {/* logo and nav links*/}
         <div className="flex items-center gap-x-5">
           <img src={logo} alt="shortly logo" className="h-6" />
-          <div className="flex-row items-center hidden gap-x-4 md:flex">
-            <button className="font-semibold hover:font-bold ">Features</button>
-            <button className="font-semibold hover:font-bold ">Pricing</button>
-            <button className="font-semibold hover:font-bold ">
-              Resources
-            </button>
-          </div>
+        </div>
+        <div className="flex-row items-center hidden gap-x-4 md:flex">
+          <button className="h-full font-semibold hover:border-black hover:border-b-2 ">
+            Home
+          </button>
+          <button className="font-semibold">About</button>
+          <button className="font-semibold">Contact</button>
+          <button className="font-semibold">Blog</button>
+          <button className="font-semibold">Career</button>
         </div>
         {/* nav links */}
         <div className="items-center hidden md:flex gap-x-4">
-          <button className="font-semibold hover:font-bold">Login</button>
-          <button className="py-1.5 px-4 bg-cyan-600 text-white font-bold rounded-3xl">
-            Sign Up
+          <button className="px-4 py-2 font-bold text-white bg-cyan-600 rounded-3xl">
+            Request invite
           </button>
         </div>
         {/* humbuger icon */}
@@ -38,20 +39,13 @@ function NavBar() {
 
         {/* mobile nav */}
         {show && (
-          <div className="absolute left-0 right-0 flex flex-col w-full px-5 text-white shadow top-16">
-            <div className="flex flex-col h-full rounded-lg p-7 bg-blue-950 gap-y-7">
-              <button className="font-semibold hover:font-bold">
-                Features
-              </button>
-              <button className="font-semibold hover:font-bold">Pricing</button>
-              <button className="font-semibold hover:font-bold">
-                Resources
-              </button>
-              <div className="w-full border border-gray-400"></div>
-              <button className="font-semibold hover:font-bold">Login</button>
-              <button className="py-1.5 px-4 bg-cyan-600 text-white font-bold rounded-3xl">
-                Sign Up
-              </button>
+          <div className="absolute left-0 right-0 flex flex-col w-full px-5 shadow top-16">
+            <div className="flex flex-col h-full bg-white rounded-lg p-7 gap-y-7">
+              <button className="font-semibold hover:font-bold">Home</button>
+              <button className="font-semibold hover:font-bold">About</button>
+              <button className="font-semibold hover:font-bold">Contact</button>
+              <button className="font-semibold hover:font-bold">blog</button>
+              <button className="font-semibold hover:font-bold">Career</button>
             </div>
           </div>
         )}
